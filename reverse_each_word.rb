fb9cb23ae9e6244c2a81c8b8e91c2df0efdca617
Chase <chaseit12@gmail.com>
2018-02-19 23:17:55 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,8 @@ def reverse_each_word(str)
     arr = str.split(' ')
     new_str = ''
     arr.each{|word|
-      new_str += word.reverse
+      new_str += word.reverse + ' '
     }
+    new_str[-1] = ''
     new_str
 end
